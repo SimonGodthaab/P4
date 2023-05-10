83f28a3b91170ad70437176c76e87e4543f31234
SimonGodthaab <93527286+SimonGodthaab@users.noreply.github.com>
2023-05-10 15:36:11 +0200

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module simple.test12345 {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-    opens simple.test12345 to javafx.fxml;
-    exports simple.test12345;
-}
